1090f66b0a868f24b660750492cd13e2bd91703f
jcamponovo <50249274+jcamponovo@users.noreply.github.com>
2019-05-13 11:38:36 +0200

    Add files via upload

--- buzzer.ino
+++ buzzer.ino
@@ -1,12 +1,12 @@
 int demiPeriodemicros = 500;
 
 void setup() {
-  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
 }
 
 void loop() {
-  digitalWrite(2,HIGH);
+  digitalWrite(3,HIGH);
   delayMicroseconds(demiPeriodemicros);
-  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
   delayMicroseconds(demiPeriodemicros);
 }
