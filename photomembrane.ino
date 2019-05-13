e4c5dc5a74e41d4483c2f0127f57da7fe9aa95c0
jcamponovo <50249274+jcamponovo@users.noreply.github.com>
2019-05-13 22:43:03 +0200

    Add files via upload

--- photomembrane.ino
+++ photomembrane.ino
@@ -1,4 +1,4 @@
-int pinPhoto = 5; 
+int pinPhoto = 3; 
 int n; 
 float R2 = 1000.;
 float U0 = 5.;
@@ -16,21 +16,22 @@ void loop(void) {
     t = micros();
     while((micros()-t)<500000){}
     t = micros();
-    for (int i=1;i<nb;i++){
+    for (int i=1;i<nb;i++){ //n-1 mesures
   
     n = analogRead(pinPhoto);
   Serial.print("[");
-  Serial.print(micros()-t);
+  Serial.print(micros()-t); //Valeur du temps depuis le début en µs
   Serial.print(",");
-  Serial.print(n); //Calcul de R1
+  Serial.print(n); //Valeur analogique mesurée
   Serial.print("]");
-  Serial.print(",");  //Attente de 500ms donc environ 2 mesures par seconde
+  Serial.print(","); 
   }
+  //Dernière mesure -on termine par un crochet fermant-
   n = analogRead(pinPhoto);
   Serial.print("[");
   Serial.print(micros()-t);
   Serial.print(",");
-  Serial.print(n); //Calcul de R1
+  Serial.print(n);
   Serial.println("]]");
   mes=false;
   }
